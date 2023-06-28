69426879ee91ae53d69a2154a6a0ab4674ebc4e9
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-28 21:43:25 +0200

    30 Pokemon Update

--- pokemon.js
+++ pokemon.js
@@ -1,31 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the current URL and extract the Pokemon ID from it
-    var pokemonId = window.location.pathname.split('/').pop().split('.')[0];
-  
-    // Fetch the Pokemon data from the server
-    fetch('Pokemon.json')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        // Find the Pokemon with the matching ID
-        var pokemon = data.find(function(p) {
-          return p.Nr === pokemonId;
-        });
-  
-        if (pokemon) {
-          // Populate the template with the Pokemon data
-          document.getElementById('pokemon-image').src = pokemon.Bild;
-          document.getElementById('pokemon-name').textContent = pokemon.Name;
-          document.getElementById('pokemon-description').textContent = pokemon.Beschreibung;
-          document.getElementById('pokemon-type').textContent = 'Typ: ' + pokemon.Typ;
-          document.getElementById('pokemon-size').textContent = 'Größe: ' + pokemon.Größe;
-        } else {
-          console.error('Pokemon not found');
-        }
-      })
-      .catch(function(error) {
-        console.error('Error:', error);
-      });
-  });
-  
+// Lese die JSON-Datei und parsiere sie
+const fs = require('fs');
+const jsonData = fs.readFileSync('data.json');
+const data = JSON.parse(jsonData);
+
+// Übergebe die Daten an die Pug-Vorlage
+const pug = require('pug');
+const template = pug.compileFile('public/includes/cards.pug');
+const renderedTemplate = template({ data });
+
+console.log(renderedTemplate);
