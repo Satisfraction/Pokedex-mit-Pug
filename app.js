47dba6d9aeefaa11c271a9f10340a761de3bcc57
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-29 21:41:01 +0200

    update

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 3000;
 
 // Set up the public directory to serve static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'public/html/index.html'));
 })
 
+
 // Define the route for the individual Pokemon pages
-app.get('/pokemon/:id', (req, res) => {
-  const pokemonId = req.params.id;
-  const pokemon = data.find(p => p.Nr === pokemonId);
-  res.render('pokemon', { pokemon });
-});
+app.get('/pokedex/:name', (req, res) => {
+  const name = req.params.name;
+  const pokemon = pokedex.find(pokemon => pokemon.Name === name);
+  res.render('pokedex', { pokemon });
+})
 
 // Start the server
-const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,12 @@
 // Lese die JSON-Datei und parsiere sie
 const fs = require('fs');
-const jsonData = fs.readFileSync('data.json');
+
+const jsonData = fs.readFileSync('Pokemon.json');
 const data = JSON.parse(jsonData);
 
 // Übergebe die Daten an die Pug-Vorlage
 const pug = require('pug');
+
 const template = pug.compileFile('public/includes/cards.pug');
 const renderedTemplate = template({ data });
 
